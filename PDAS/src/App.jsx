a22b0862f9f85901bf2f91cb62567d4f9ecfa14b
Kevin Andres Ortiz Merchan - vn559a9 <kevinandresortizmerchan@outlook.com>
2023-12-22 09:14:45 -0500

    images App
    added images into App

--- PDAS/src/App.jsx
+++ PDAS/src/App.jsx
@@ -1,12 +1,17 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+
+
+            <ProfileCard title="Alexa" handle= "@alexa" image={AlexaImage} />
+            <ProfileCard title="Siri" handle= "@siri"  image={SiriImage} />
+            <ProfileCard title="Cortana" handle= "@cortana" image={CortanaImage} />
         </div>
     )
 }
--- PDAS/src/ProfileCard.jsx
+++ PDAS/src/ProfileCard.jsx
@@ -1,5 +1,17 @@
-function ProfileCard(){
-    return <div>Profile Card</div>
+import PropTypes from 'prop-types';
+function ProfileCard({title, handle, image}){
+    return (
+        <div>
+            <img src={image} alt='pda logo' />
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </div>
+    )
 }
 
+ProfileCard.propTypes = {
+    title: PropTypes.string,
+    handle: PropTypes.string,
+    image: PropTypes.any
+}
 export default ProfileCard;
