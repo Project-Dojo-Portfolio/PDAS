b2f46b68dcd72d8583c7fb587524bafc5be6a7bd
Kevin Andres Ortiz Merchan - vn559a9 <kevinandresortizmerchan@outlook.com>
2023-12-26 14:28:37 -0500

    added BUM CSS
    added custom classes in ProfileCard component

--- PDAS/src/App.jsx
+++ PDAS/src/App.jsx
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
@@ -6,12 +7,40 @@ import SiriImage from './images/siri.png';
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <h1 className='title'>Personal Digital Assistants</h1>
+                </div>
+            </section>
 
-
-            <ProfileCard title="Alexa" handle= "@alexa" image={AlexaImage} />
-            <ProfileCard title="Siri" handle= "@siri"  image={SiriImage} />
-            <ProfileCard title="Cortana" handle= "@cortana" image={CortanaImage} />
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCard 
+                                title="Alexa" 
+                                handle= "@alexa" 
+                                image={AlexaImage} 
+                                description='Alexa was created by Amazon and helps you buy things' />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard 
+                                title="Siri" 
+                                handle= "@siri"  
+                                image={SiriImage} 
+                                description='Siri was made by Apple is being phased out'/>
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard 
+                                title="Cortana" 
+                                handle= "@cortana" 
+                                image={CortanaImage}
+                                description='Cortana was made by Microsoft who knows what it does?' />
+                        </div>
+                    </div>
+                </section>
+            </div>
+         
         </div>
     )
 }
--- PDAS/src/ProfileCard.jsx
+++ PDAS/src/ProfileCard.jsx
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
-function ProfileCard({title, handle, image}){
+function ProfileCard({title, handle, image, description}){
     return (
-        <div>
-            <img src={image} alt='pda logo' />
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className='card'>
+            <div className='card-image'>
+                <figure className='image is-1by1'> 
+                    <img src={image} alt='pda logo' />
+                </figure>
+            </div>
+            <div className='card-content'>
+                <div className='media-content'>
+                    <div className='title is-4'>Title is {title}</div>
+                    <div className='subtitle is-6'>Handle is {handle}</div>
+                    <div className='suptitle is-6'>{description}</div>
+                </div>
+            </div>
         </div>
     )
 }
@@ -12,6 +21,7 @@ function ProfileCard({title, handle, image}){
 ProfileCard.propTypes = {
     title: PropTypes.string,
     handle: PropTypes.string,
+    description: PropTypes.string,
     image: PropTypes.any
 }
 export default ProfileCard;
