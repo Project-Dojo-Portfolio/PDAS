7140b965032908169aa2c22c58036f863afe4941
Kevin Andres Ortiz Merchan - vn559a9 <kevinandresortizmerchan@outlook.com>
2024-01-08 09:28:21 -0500

    images
    added images into animals projects

--- animals/src/AnimalShow.jsx
+++ animals/src/AnimalShow.jsx
@@ -1,7 +1,38 @@
+import './AnimalShow.css';
 import PropTypes from 'prop-types';
 
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+import { useState } from 'react';
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+};
+
 function AnimalShow({ type }) {
-    return (<div>{type}</div>);
+    
+    const [clicks, setClicks] = useState(0);
+    
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+
+    return (
+            <div onClick={handleClick}>
+                <img alt='animal' src={svgMap[type]} width='500px' />
+                <img alt='heart' src={heart} style={{ width: 10 + 10 * clicks }} />
+            </div>
+        );
 }
 
 AnimalShow.propTypes = {
