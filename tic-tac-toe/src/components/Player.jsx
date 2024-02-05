23c074afee09a95d0ad0c98182016d4012a6ab86
Kevin Andres Ortiz Merchan - vn559a9 <kevinandresortizmerchan@outlook.com>
2024-02-05 14:53:52 -0500

    two way binding input text

--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol}){
+    const [playerName, setPlayerName] = useState(initialName);
     const [ isEdition, setIsEditing ] = useState(false);
 
-    let playerName = <span className="player-name">{name}</span>
+    let editablePlayerName = <span className="player-name">{playerName}</span>
 
     function handleClickEditButton(){
         setIsEditing(editing => !editing);
+
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
     }
 
     if(isEdition) {
-        playerName = <input type="text" required value={name} />
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
     }
     return(
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleClickEditButton}>{isEdition ? 'Save' : 'Edit'}</button>
